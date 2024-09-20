17d851ece3c5ce5cec7a47de4a1d605c252d42d2
JeremieV <jeremievaney@gmail.com>
2024-09-20 13:59:22 +0100

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,106 @@
-import Image from "next/image";
+// import Image from "next/image";
+
+import Link from "next/link";
+// import { useState } from "react";
+
+const feeds = [
+  
+]
+
+const data = [
+  {
+    id: 1,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+  {
+    id: 2,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+  {
+    id: 3,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+  {
+    id: 4,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+  {
+    id: 5,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+  {
+    id: 6,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+  {
+    id: 7,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+  {
+    id: 8,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+  {
+    id: 9,
+    title: "The future of the web is in your hands",
+    url: "http://blog.ycombinator.com/the-future-of-the-web-is-in-your-hands/",
+    points: 134,
+    user: "dhouston"
+  },
+]
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // const [state, setState] = useState(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+  return (
+    <div className="max-w-[60rem] m-auto bg-orange-50 my-2">
+      <nav className="bg-orange-500 px-2 flex">
+        <span>Hacker News Clone</span>
+        <div className="grow"></div>
+        <Link href="/login">Login</Link>
+      </nav>
+      <div className="px-2">
+        <ol className="list-inside list-decimal">
+          {data.map((item) => (
+            <li key={item.id} className="gap-4 list-item list-decimal">
+              <a href={item.url} className="inline-flex flex-col">
+                <div>
+                  <span>{item.title} </span>
+                  <span className="hover:underline">({new URL(item.url).hostname})</span> <br />
+                </div>
+                <small>{item.points} points by {item.user} x minutes/hours ago</small>
+              </a>
+            </li>
+          ))}
+        </ol>
+        <p className="pt-4">More</p>
+      </div>
+      <footer className="border-t-2 border-orange-500 px-2 text-center py-4">
+        <p>Hacker News Clone, 2024</p>
       </footer>
     </div>
   );
