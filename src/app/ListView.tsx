467d153413cdb04f5fcf82a940a63d4f2c6b3c3e
JeremieV <jeremievaney@gmail.com>
2024-10-07 17:09:17 +0100

    fixed link hit box in list view

--- src/app/ListView.tsx
+++ src/app/ListView.tsx
@@ -5,22 +5,13 @@ export default function ListView({ currentStories }: { currentStories: Story[], 
   return (
     <div className="overflow-hidden mb-4">
       {currentStories.map((story) => (
-        <a
-          key={story.id}
-          href={story.url}
-          target="_blank"
-          title={story.title}
-          rel="noopener noreferrer"
-          className="flex py-2"
-        >
-          <div className='flex flex-col'>
-            <span className='hover:underline text-foreground line-clamp-3'>{story.title}</span>
-            <div className='text-muted-foreground text-sm flex'>
-              <a href={`/feed/${encodeURIComponent(story.feedUrl)}`} title={story.feedTitle} className="hover:text-primary transition-colors">{displayUrl(story.url)}</a>
-              <span className="mx-1">{`•`}</span><span>{displayTimeAgo(story.published)}</span>
-            </div>
+        <div key={story.id} className='flex flex-col py-2 items-start'>
+          <a href={story.url} target="_blank" title={story.title} rel="noopener noreferrer" className='hover:underline text-foreground line-clamp-3'>{story.title}</a>
+          <div className='text-muted-foreground text-sm flex'>
+            <a href={`/feed/${encodeURIComponent(story.feedUrl)}`} title={story.feedTitle} className="hover:text-primary transition-colors">{displayUrl(story.url)}</a>
+            <span className="mx-1">{`•`}</span><span>{displayTimeAgo(story.published)}</span>
           </div>
-        </a>
+        </div>
       ))}
     </div>
   )
