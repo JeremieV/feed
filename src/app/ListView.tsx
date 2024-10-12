9d05ad76499198db07e9ef8afcccad064d32d5ae
JeremieV <jeremievaney@gmail.com>
2024-10-12 23:54:34 +0100

    fix: type error

--- src/app/ListView.tsx
+++ src/app/ListView.tsx
@@ -5,11 +5,11 @@ export default function ListView({ currentStories }: { currentStories: Story[], 
   return (
     <div className="overflow-hidden mb-4">
       {currentStories.map((story) => (
-        <div key={story.id} className='flex flex-col py-2 px-[1px] items-start'>
+        <div key={story.url} className='flex flex-col py-2 px-[1px] items-start'>
           <a href={story.url} target="_blank" title={story.title} rel="noopener noreferrer" className='hover:underline text-foreground line-clamp-3'>{story.title}</a>
           <div className='text-muted-foreground text-sm flex'>
-            <a href={`/feed/${encodeURIComponent(story.feedUrl)}`} title={story.feedTitle} className="hover:text-primary transition-colors">{displayUrl(story.url)}</a>
-            <span className="mx-1">{`•`}</span><span>{displayTimeAgo(story.published)}</span>
+            <a href={`/feed/${encodeURIComponent(story.feedUrl)}`} title={story.title} className="hover:text-primary transition-colors">{displayUrl(story.url)}</a>
+            <span className="mx-1">{`•`}</span><span>{displayTimeAgo(story.pubDate)}</span>
           </div>
         </div>
       ))}
