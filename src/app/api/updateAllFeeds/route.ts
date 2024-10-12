db2d7a3093983637758074b058fb9fc97d1992ca
JeremieV <jeremievaney@gmail.com>
2024-10-12 23:28:30 +0100

    attempting to fix cron job

--- src/app/api/updateAllFeeds/route.ts
+++ src/app/api/updateAllFeeds/route.ts
@@ -4,8 +4,11 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   if (process.env.NODE_ENV !== "development" && req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    console.warn("Unauthorized request to update all feeds.");
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
+
+  console.info("Running cron job to update all feeds...");
 
   // select all feeds from the database
   const allFeeds = await db.select({ url: feeds.url })
