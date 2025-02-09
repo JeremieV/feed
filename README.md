899d9f29bf74d926e9cd3e79ba02036a161fd2cc
JeremieV <jeremievaney@gmail.com>
2025-02-09 18:42:17 +0000

    archiving the project

--- README.md
+++ README.md
@@ -2,34 +2,35 @@
 
 The open source link aggregator.
 
-This is my side project at the moment.
+This used to be a side project of mine, now unmaintained.
 
 ## Roadmap 
 
 BUG FIXES:
 - fix feed items duplication
 
-- review the youtube etc compatibility (and activitypub)
+- transfer db to neon
 - redo the search bar
+- review the youtube etc compatibility (and activitypub)
 - implement upvotes (add an upvote_count to links), user profiles (name image description), and history
-- recommendation algo (eg 50% most upvoted, 30% following, 20% random (to start))
-- import/export OPML
+- implement a most upvoted (all time) and "hot" category <- upvotes divided by a power of the age of the link (can have different freshness parameters)
 <!--  -->
-- announce project somewhere
-- put some interested people in a group (discord/slack)
+<!-- - announce project somewhere
+- put some interested people in a group (discord/slack) -->
 <!--  -->
-- branding
+<!-- - branding
   - logo / favicon
-  - metadata
+  - metadata -->
 <!--  -->
+- import/export OPML
 - limit the time window of the random stories
 - curate "topics" better than the AI generated ones
 - display skeleton while loading instead of loading indicator (grid & list view)
 - let users create combined feeds
 - fix the tailwind css pattern which is over optimistic
-- choose a license (I am thinking about https://choosealicense.com/licenses/unlicense/)
+<!-- - choose a license (I am thinking about https://choosealicense.com/licenses/unlicense/) -->
 <!--  -->
-- launch
+<!-- - launch -->
 
 ## Changelog (most recent first)
 
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -17,7 +17,7 @@ function InputField() {
   const [showSuggestions, setShowSuggestions] = useState(false)
   const inputRef = useRef<HTMLInputElement>(null)
   const suggestionsRef = useRef<HTMLDivElement>(null)
-  const [indexFocused, setIndexFocused] = useState(0)
+  // const [indexFocused, setIndexFocused] = useState(0)
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -40,63 +40,20 @@ function InputField() {
     }
   }, [searchTerm])
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
-    setSearchTerm(value)
-    setShowSuggestions(true)
-  }
-
   const router = useRouter();
-  async function gotoFeedPage(url: string) {
+  function gotoFeedPage(url: string) {
     setShowSuggestions(false)
+    console.log('navigating to', url)
     router.push(`/feed/${encodeURIComponent(url)}`)
   }
 
-  // TODO implement up/down keys navigation, and 'Enter' to submit
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-
-      // search bar shortcut
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault()
-        inputRef.current?.focus()
-      }
-
-      // keys to navigate the suggestions
-      // if (showSuggestions && suggestions?.length) {
-      // }
-      if (e.key === "ArrowDown") {
-        setIndexFocused((prev) => (prev + 1))
-      }
-      if (e.key === "ArrowUp") {
-        setIndexFocused((prev) => (prev - 1))
-      }
-      if (e.key === "Enter") {
-        console.log('enter pressed')
-        if (indexFocused === 0 && isValidUrl(searchTerm)) {
-          // TODO url to RSS conversion and real search
-          const url = urlToRSS(searchTerm)
-          gotoFeedPage(searchTerm)
-        } else if (indexFocused !== 0) {
-          const focused = suggestions?.[indexFocused].url;
-          if (focused) {
-            gotoFeedPage(focused)
-          }
-        }
-      }
-    }
-    document.addEventListener("keydown", down)
-    return () => document.removeEventListener("keydown", down)
-  }, [])
-
   function isValidUrl(url: string) {
-    try { new URL(url) } catch { return false }
-    return true;
+    try { new URL(url); return true } catch { return false }
   }
 
   const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
-  const { isPending, data: suggestions } = useQuery({
+  const { data: suggestions } = useQuery({
     queryKey: ['feeds-search', debouncedSearchTerm],
     queryFn: async () => {
       return await searchFeeds(debouncedSearchTerm)
@@ -113,9 +70,37 @@ function InputField() {
           type="text"
           placeholder="Search"
           value={searchTerm}
-          onChange={handleInputChange}
+          onChange={(e) => {
+            const value = e.target.value
+            console.log(value)
+            setSearchTerm(value)
+            setShowSuggestions(true)
+          }}
+          onKeyDown={async (e) => {
+            // shortcut
+            if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+              e.preventDefault()
+              inputRef.current?.focus()
+            }
+            // search
+            if (e.key === "Enter") {
+              console.log('enter pressed')
+              if (isValidUrl(searchTerm)) {
+                console.log('url', searchTerm)
+                const url = await urlToRSS(searchTerm)
+                console.log(url)
+                gotoFeedPage(url)
+                return
+              } else {
+                console.log('not url', searchTerm)
+                // TODO replace this wih search
+                gotoFeedPage(searchTerm)
+              }
+            }
+          }}
           onFocus={() => setShowSuggestions(true)}
-          className="mx-0"></Input>
+          className="mx-0">
+        </Input>
         {
           showSuggestions && (
             <div
@@ -125,10 +110,10 @@ function InputField() {
               {suggestions?.length ? (
                 suggestions.map((s, index) => (
                   <a
-                    onMouseOver={() => setIndexFocused(index)}
+                    // onMouseOver={() => setIndexFocused(index)}
                     href={`/feed/${encodeURIComponent(s.url)}`}
                     key={index}
-                    className={`px-4 py-2 ${index === (indexFocused % suggestions.length) ? 'bg-muted' : ''} cursor-pointer flex items-center`}
+                    className={`px-4 py-2 cursor-pointer flex items-center`} // ${index === (indexFocused % suggestions.length) ? 'bg-muted' : ''}
                   >
                     <Search className="h-4 w-4 mr-2 text-muted-foreground" />
                     {s.title}
@@ -153,32 +138,6 @@ export default function SearchBar() {
   const [searchBarOpen, setSearchBarOpen] = useAtom(searchBarOpenAtom)
 
   return (
-    // <>
-    //   <div className="lg:hidden grow"></div>
-    //   <Button variant="outline" className="gap-2 md:w-full max-w-2xl justify-end md:justify-between mx-auto" onClick={() => setOpen(true)}>
-    //     <span className="sr-only md:not-sr-only min-w-80 text-left text-muted-foreground">Search</span>
-    //     <div className="grow hidden lg:block"></div>
-    //     <kbd className="hidden lg:inline-flex pointer-events-none h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-    //       <span className="text-xs">⌘</span>K
-    //     </kbd>
-    //     <Separator orientation="vertical" className="hidden lg:block" />
-    //     <Search size={20} strokeWidth={1.2} className="md:mx-1" />
-    //   </Button>
-    //   {/* CommandDialog is giving a lot of trouble. Maybe use a select instead? */}
-    //   <CommandDialog open={open} onOpenChange={setOpen}>
-    //     <CommandInput placeholder={placeholder} onInput={v => setInputValue(v?.currentTarget.value)} onKeyDown={async (e) => e.code === 'Enter' && isValidUrl(inputValue) && gotoFeedPage(await urlToRSS(inputValue))} />
-    //     <CommandList>
-    //       <CommandEmpty>{isPending ? 'Loading...' : 'No results found.'}</CommandEmpty>
-    //       {suggestions?.map(s =>
-    //         <CommandItem key={s.url} className="rounded-none hover:cursor-pointer" onSelect={() => gotoFeedPage(s.url)}>
-    //           {/* eslint-disable-next-line @next/next/no-img-element */}
-    //           <img src={faviconUrl(s.link ?? '')} alt="" className="aspect-square w-6 h-6 rounded-md mr-4" />
-    //           <span>{s.title}</span>
-    //         </CommandItem>
-    //       )}
-    //     </CommandList>
-    //   </CommandDialog>
-    // </>
     <>
       <div className="w-full hidden lg:block">
         <div className="flex gap-2 mx-auto md:w-full max-w-2xl justify-end md:justify-between">
